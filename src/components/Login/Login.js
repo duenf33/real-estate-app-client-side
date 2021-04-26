9f8fc215979f1b629f86a61cd8e603474529fdf9
fernando duenas <fernando39615@aim.com>
2021-04-26 15:57:23 -0400

    working on login client side

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { makeStyle } from "@material-ui/core/styles"
+import {
+	FormControl,
+	FormHelperText,
+	Input,
+	InputLabel,
+	Button,
+	CircularProgress,
+	Snackbar,
+	Grid,
+} from '@material-ui/core'
+import MuiAlert from "@material-ui/lab/Alert"
+
+import useInputHooks from '../hooks/useInputHooks'
+import useEmailHooks from '../hooks/useEmailHooks'
+import usePasswordHooks from "../hooks/usePasswordHooks"
+
+const useStyles = makeStyle((theme) => ({
+	root: {
+		"& > *": {
+			margin: theme.spacing(1),
+			width: 350,
+		},
+	},
+}))
 
 function Login() {
+	const classes = useStyles();
+
+	const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
 	return <div>Login</div>;
 }
 
